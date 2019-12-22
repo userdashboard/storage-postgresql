0a420f23ccd1cdc92777f547bb5d93e6732b91c6
benlowry <benlowry@outlook.com>
2019-12-22 14:11:18 +0000

    WIP setup db

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/
 const configParse = require('pg-connection-string').parse
 const connectionConfig = configParse(connectionString)
 const fs = require('fs')
+const path = require('path')
 const pg = require('pg')
 const pool = new pg.Pool(connectionConfig)
-pool.query(fs.readFileSync('./setup.sql'))
+pool.query(fs.readFileSync(path.join(__dirname, 'setup.sql')))
