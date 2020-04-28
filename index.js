2ee5bc94777669ae5462a0ad1a712e917cfa0f80
benlowry <benlowry@outlook.com>
2020-04-28 12:00:55 -0600

    Read setup.sql when nested as module

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ module.exports = {
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres'
 const configParse = require('pg-connection-string').parse
 const connectionConfig = configParse(connectionString)
+const fs = require('fs')
+const path = require('path')
 const pg = require('pg')
+
+let setupSQLFile = path.join(__dirname, 'setup.sql')
+if (!fs.existsSync(setupSQLFile)) {
+  setupSQLFile = path.join(__dirname, 'node_modules/@userdashboard/storage-postgresql/setup.sql')
+}
+
 const pool = new pg.Pool(connectionConfig)
-pool.query(require('./setup.sql').toString())
+pool.query(setupSQLFile)
