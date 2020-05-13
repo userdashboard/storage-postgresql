067d2b3fcbe6b8735fb9fc1171e3d813ecf21de0
benlowry <benlowry@outlook.com>
2020-05-13 12:04:58 -0600

    Release pool used to ensure tables exist

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-module.exports = {
-  Storage: require('./src/storage.js'),
-  StorageList: require('./src/storage-list.js')
-}
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres'
-const configParse = require('pg-connection-string').parse
-const connectionConfig = configParse(connectionString)
+const databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres'
+const connectionString = require('pg-connection-string')
+const connectionConfig = connectionString.parse(databaseURL)
 const fs = require('fs')
 const path = require('path')
 const pg = require('pg')
@@ -13,5 +9,15 @@ if (!fs.existsSync(setupSQLFile)) {
   setupSQLFile = path.join(global.applicationPath, 'node_modules/@userdashboard/storage-postgresql/setup.sql')
 }
 setupSQLFile = fs.readFileSync(setupSQLFile).toString()
-const pool = new pg.Pool(connectionConfig)
-pool.query(setupSQLFile)
+try {
+  const pool = new pg.Pool(connectionConfig)
+  pool.query(setupSQLFile, () => {
+    pool.end()
+  })
+} catch (error) {
+}
+
+module.exports = {
+  Storage: require('./src/storage.js'),
+  StorageList: require('./src/storage-list.js')
+}
